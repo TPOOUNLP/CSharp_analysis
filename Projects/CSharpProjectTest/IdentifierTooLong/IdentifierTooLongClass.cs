973a3d2abe08725cf2f54758dae2e87f7df6b6be
Nahuel Aparicio <nahuel94@gmail.com>
2019-07-08 15:07:15 -0300

    Added 2 classes to Test project

---
+++ Projects/CSharpProjectTest/IdentifierTooLong/IdentifierTooLongClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CsharpProject
+{
+    class IdentifierTooLongClass
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+            string messageForTheUnworthyWarriorsOfTheNeverland = "The number is " + number;
+            Console.WriteLine(messageForTheUnworthyWarriorsOfTheNeverland);
+        }
+    }
+}
